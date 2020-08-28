10d87cda8cdaaa94352fc937202e7cc5373766bc
ErenYarar <eren2yarar1@gmail.com>
2020-08-28 16:09:34 +0300

    Create bilgilerim.dart

---
+++ bilgilerim.dart
@@ -0,0 +1,13 @@
+void main() 
+{
+	String name = "Eren";
+  String surname = "Yarar";
+  int age = 22;
+  int dogum_yili = 1998;
+  double oran = 33.3;
+  print("Ad => " + name);
+  print("Soyad => " + surname);
+  print("Yas => " + age.toString());
+  print("Dogum yili => " + dogum_yili.toString());
+  print("Sevilen sayi => " + oran.toString());
+}
